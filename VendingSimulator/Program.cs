1bff2130a7c024ca493760c35420ee4a38f60975
tuckernemcek21@gmail.com <tuckernemcek21@gmail.com>
2019-05-16 10:24:07 -0600

    Added a button class

---
+++ VendingSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VendingSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
